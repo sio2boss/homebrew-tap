4eeb561292e1106886092ff50635685cf69d1832
sio2boss <2815132+sio2boss@users.noreply.github.com>
2025-09-15 19:19:46 -0400

    Adding tunnel9 v1.0.3

--- Formula/tunnel9.rb
+++ Formula/tunnel9.rb
@@ -1,26 +1,26 @@
 class Tunnel9 < Formula
   desc "Terminal user interface (TUI) for managing SSH tunnels"
   homepage "https://github.com/sio2boss/tunnel9"
-  version "1.0.2"
+  version "1.0.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/sio2boss/tunnel9/releases/download/v#{version}/tunnel9-v#{version}-apple-arm64.tar.gz"
-      sha256 "0567f0032c212ecff63d3b2b4df907f8c8c298c0f1e19e046fa6eb13434f4126"
+      sha256 "793498df905405df15632b993282bc8cc01b48acefc9397eafbf60989b1de22b"
     else
       url "https://github.com/sio2boss/tunnel9/releases/download/v#{version}/tunnel9-v#{version}-apple-amd64.tar.gz"
-      sha256 "e933fded44e603c5f2e656eff4896e6aa84e1c553cf6af20d3a5baea169a3580"
+      sha256 "fc1a4da1aac2cc836b8e8f3dc0930f5b6df2ea8ae9ea73f556e1c7b94ae2a31f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/sio2boss/tunnel9/releases/download/v#{version}/tunnel9-v#{version}-linux-arm64.tar.gz"
-      sha256 "b118b9817404c1081923a41b5ef34f918db7c96a520bb0b26a83501c504f6336"
+      sha256 "e7801edc94474aba9b33fb21015bd54983ae31bbbf8e9098fd4b321e4d95eb45"
     else
       url "https://github.com/sio2boss/tunnel9/releases/download/v#{version}/tunnel9-v#{version}-linux-amd64.tar.gz"
-      sha256 "e86e0602b24a9e8c1dba5bc9332f2f9bfd4a10a97ddd8cbd628d74d85569bba8"
+      sha256 "888757295fc82ddea248bce876ca5440e2b7a193e5ba5d809485378b4c1dec6e"
     end
   end
 
