317de94f91ccec121aa88b0616e2ed6a2d818dcd
sio2boss <2815132+sio2boss@users.noreply.github.com>
2026-02-05 10:19:48 -0500

    atlas9 v0.9.0 pre-release (binary filename fix)

--- Formula/atlas9.rb
+++ Formula/atlas9.rb
@@ -6,21 +6,21 @@ class Atlas9 < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9-v0.9.0-apple-arm64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9_darwin_arm64"
+      sha256 "8b003eda9f869969ca6c8410e69bc921b6937ca9d87d2444c2b583fe7e33128f"
     else
-      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9-v0.9.0-apple-amd64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9_darwin_amd64"
+      sha256 "ff058e7a7de74a8ba08620ef2fe6bfeaf94646bf4c486262c7283f595c65681b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9-v0.9.0-linux-arm64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9_linux_arm64"
+      sha256 "8dbaed8cfc0d5b667154bcdeb99bcce5c4db5be45b4e1151d6ae910fcd612a85"
     else
-      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9-v0.9.0-linux-amd64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9_linux_amd64"
+      sha256 "a04d10430aafef5ce85f09b222759a7c5f7d8aeb661aa52ed790d6b57bb2013e"
     end
   end
 
