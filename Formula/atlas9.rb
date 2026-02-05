1e0286408872c95a87a5954caf77bed253b0d471
sio2boss <2815132+sio2boss@users.noreply.github.com>
2026-02-05 10:13:16 -0500

    atlas9 v0.9.0 pre-release

---
+++ Formula/atlas9.rb
@@ -0,0 +1,34 @@
+class Atlas9 < Formula
+  desc "Download and install files from the web with automatic extraction and installation"
+  homepage "https://github.com/sio2boss/atlas9"
+  version "0.9.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9-v0.9.0-apple-arm64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    else
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9-v0.9.0-apple-amd64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9-v0.9.0-linux-arm64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    else
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9-v0.9.0-linux-amd64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    end
+  end
+
+  def install
+    bin.install "atlas9"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/atlas9 --version")
+  end
+end
