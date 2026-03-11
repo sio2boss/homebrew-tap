59702bd210a011264401c3414b1f8b7f58239b73
sio2boss <2815132+sio2boss@users.noreply.github.com>
2026-03-11 16:14:25 -0400

    atlas9 v0.9.1 release

--- Formula/atlas9.rb
+++ Formula/atlas9.rb
@@ -1,26 +1,26 @@
 class Atlas9 < Formula
-  desc "Terminal user interface (TUI) for using atlas to manage your database"
+  desc "Download and install files from the web with automatic extraction and installation"
   homepage "https://github.com/sio2boss/atlas9"
-  version "0.9.0"
+  version "0.9.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9_darwin_arm64.tar.gz"
-      sha256 "ed236c6c31181ba9ed9c95f78d8038eefe3ed09f84544d92e7c7fcec5fcdd73f"
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.1/atlas9_darwin_arm64.tar.gz"
+      sha256 "ba5c298c442bf7e487a5a2251e42a332ea085135ea803358c26ebd2ae8340b3d"
     else
-      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9_darwin_amd64.tar.gz"
-      sha256 "5077372e0a6f8eb1a57ec2594e084604c705a84f587a1bc995c51384890ceab1"
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.1/atlas9_darwin_amd64.tar.gz"
+      sha256 "0e610e606fb80f33edc500dc93ac27c43ddc18885474ce1f292f229f6996d6f6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9_linux_arm64.tar.gz"
-      sha256 "91b15cfb4134501a54424076e688c574aeece5337e6cd9f6065ebf5e7ee860bb"
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.1/atlas9_linux_arm64.tar.gz"
+      sha256 "997a2f3099d676c365a3fbf05435ca48543c3eb1749ec25e16c42cf0e8388d25"
     else
-      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.0/atlas9_linux_amd64.tar.gz"
-      sha256 "03fd5b4e42687ee3713c579d72e59a56935e2c1abcb80e842ce5bd78915869bd"
+      url "https://github.com/sio2boss/atlas9/releases/download/v0.9.1/atlas9_linux_amd64.tar.gz"
+      sha256 "63eb85bf3fbb1a5429c1ebfd90697cc93656b604032c6680c3c69971c350e58e"
     end
   end
 
@@ -29,6 +29,6 @@ class Atlas9 < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/atlas9 --help")
+    assert_match version.to_s, shell_output("#{bin}/atlas9 --version")
   end
 end
